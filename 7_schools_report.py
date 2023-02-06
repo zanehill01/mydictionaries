7a7c607ab7cb1597cf0a516ec41869eaf5da8919
zanehill01 <zane_hill1@baylor.edu>
2023-02-06 14:18:04 -0600

    Working on 7

--- 7_schools_report.py
+++ 7_schools_report.py
@@ -14,6 +14,37 @@ Copy that info here:
 Display report for all universities that have a graduation rate for Women over 50%
 Display report for all universities that have a total price for in-state students living off campus over $50,000
 
-
-
 """
+
+import json
+
+infile = open('school_data.json', 'r')
+
+schools = json.load(infile)
+conference_schools = [372, 108, 107, 130]
+
+print(type(schools))
+
+# How many schools are in this file?
+
+print(len(schools))
+
+# Display report for all universities that have a graduation rate for Women over 50%
+# Display report for all universities that have a total price for in-state students living off campus over $50,000
+
+for row in schools:
+    if row['NCAA']['NAIA conference number football (IC2020)'] in conference_schools:
+        if row['Graduation rate  women (DRVGR2020)'] > 80:
+            print(row["instnm"])
+            print(row['Graduation rate  women (DRVGR2020)'])
+
+
+for row in schools:
+
+    total = row['Total price for in-state students living off campus (not with family)  2020-21 (DRVIC2020)']
+    school = row["instnm"]
+
+    print(total, school)
+
+    # if total > 50000:
+    #    print(school, ',', 'Has a total price for in-state students living off campus of,')
