98c6d345b26fa7c3f583ee6c438c4bf91b08723b
zanehill01 <zane_hill1@baylor.edu>
2023-02-07 20:12:22 -0600

    7 Finished

--- 7_schools_report.py
+++ 7_schools_report.py
@@ -23,28 +23,27 @@ infile = open('school_data.json', 'r')
 schools = json.load(infile)
 conference_schools = [372, 108, 107, 130]
 
-print(type(schools))
-
 # How many schools are in this file?
 
-print(len(schools))
+# print(len(schools))
 
 # Display report for all universities that have a graduation rate for Women over 50%
-# Display report for all universities that have a total price for in-state students living off campus over $50,000
+
+print('\nAll Universities that have a Graduation Rate for Women over 50%\n')
 
 for row in schools:
     if row['NCAA']['NAIA conference number football (IC2020)'] in conference_schools:
-        if row['Graduation rate  women (DRVGR2020)'] > 80:
-            print(row["instnm"])
-            print(row['Graduation rate  women (DRVGR2020)'])
+        if row['Graduation rate  women (DRVGR2020)'] > 50:
+            print(row["instnm"], ', ',
+                  row['Graduation rate  women (DRVGR2020)'], '%', sep='')
 
+# Display report for all universities that have a total price for in-state students living off campus over $50,000
+
+print('\nAll Universities that have a Total Price for in-state Students living Off Campus Over 50,000$\n')
 
 for row in schools:
-
-    total = row['Total price for in-state students living off campus (not with family)  2020-21 (DRVIC2020)']
-    school = row["instnm"]
-
-    print(total, school)
-
-    # if total > 50000:
-    #    print(school, ',', 'Has a total price for in-state students living off campus of,')
+    if row['NCAA']['NAIA conference number football (IC2020)'] in conference_schools:
+        total = row['Total price for in-state students living off campus (not with family)  2020-21 (DRVIC2020)']
+        if type(total) == int:
+            if total > 50000:
+                print(total, '$ ', row["instnm"], sep='')
